355edd62fad5acd866f30cef50fa7cd5153b2c04
randychoi95 <jhc130833@naver.com>
2020-02-12 09:41:06 +0900

    매장찾기에서 PC리스트 수정 회원가입 기능

--- app/src/main/java/com/ezen/MyPcApplication/First_View/FirstActivity.java
+++ app/src/main/java/com/ezen/MyPcApplication/First_View/FirstActivity.java
@@ -1,13 +1,9 @@
 package com.ezen.MyPcApplication.First_View;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,12 +12,7 @@ import android.widget.Toast;
 
 import com.ezen.MyPcApplication.After_Main.MainActivity;
 import com.ezen.MyPcApplication.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class FirstActivity extends AppCompatActivity {
 
