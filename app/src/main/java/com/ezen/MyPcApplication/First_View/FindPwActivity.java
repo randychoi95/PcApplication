d41d3491c67805c7ea764289bab1c93309ac731b
han788290 <han788290@gmail.com>
2020-02-18 17:50:27 +0900

    ConvertPwActivity(비밀번호 재설정) FIndPwActivity(비밀번호 재설정 이메일)

--- app/src/main/java/com/ezen/MyPcApplication/First_View/FindPwActivity.java
+++ app/src/main/java/com/ezen/MyPcApplication/First_View/FindPwActivity.java
@@ -1,21 +1,74 @@
 package com.ezen.MyPcApplication.First_View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ezen.MyPcApplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FindPwActivity extends AppCompatActivity {
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    TextInputLayout id_input;
+    Button find_btn_ok;
+    Button find_btn_cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_findpw);
 
+        // 비밀번호 찾기 확인버튼
+        find_btn_ok = findViewById(R.id.find_btn_ok2);
+        find_btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findPW();
+            }
+        });
+
+        // 비밀번호 찾기 취소 버튼
+        find_btn_cancel = findViewById(R.id.find_btn_cancel2);
+        find_btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
 
     }//oncreate
 
+    // 비밀번호 재설정 이메일 보내기.
+    public void findPW() {
+        id_input = findViewById(R.id.id_input_layout);
+        EditText editText = id_input.getEditText();
+        String email = editText.getText().toString();
+
+        auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "재설정 이메일 전송완료 \n재설정 이메일을 통해 비밀번호를 변경해주세요!!", Toast.LENGTH_LONG).show();
+                            finish();
+                        }
+                    }
+                });
+
+
+    }
 
 }//class
 
--- app/src/main/java/com/ezen/MyPcApplication/Side_Navigation/Person/ConvertPwActivity.java
+++ app/src/main/java/com/ezen/MyPcApplication/Side_Navigation/Person/ConvertPwActivity.java
@@ -1,22 +1,69 @@
 package com.ezen.MyPcApplication.Side_Navigation.Person;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ezen.MyPcApplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ConvertPwActivity extends AppCompatActivity {
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();  // 파이어베이스 인증
+    FirebaseUser currentUser = auth.getCurrentUser(); // 사용자의 프로필
+    EditText editTextPw;
+    EditText editTextPw2;
+    Button btn_ok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_convert_pw);
 
+        editTextPw = findViewById(R.id.editTextPw);
+        editTextPw2 = findViewById(R.id.editTextPw2);
+        btn_ok = findViewById(R.id.btn_ok);
+
         // 툴바에 뒤로가기 백버튼 생성
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newPassword = editTextPw.getText().toString();
+                String RePassword = editTextPw2.getText().toString();
+                if ( newPassword.equals(RePassword) ) {
+                    Log.e("Han", "값: " + newPassword + "," + RePassword + " 길이: " + newPassword.length() + "," + RePassword.length());
+                    currentUser.updatePassword(newPassword).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Log.e("Han", "test1");
+                            if ( task.isSuccessful() ){
+                                Log.e("Han", "test2");
+                                Toast.makeText(getApplicationContext(), "비밀번호가 변경되었습니다.", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(getApplicationContext(), "비밀번호가 다릅니다.", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
+
 
     // 뒤로가기 버튼 누를 시에 이벤트 동작
     @Override
