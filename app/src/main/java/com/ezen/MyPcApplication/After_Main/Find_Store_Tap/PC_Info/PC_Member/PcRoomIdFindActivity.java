320eecd4969b0013a021e7bcbd50d332356839c8
randychoi95 <jhc130833@naver.com>
2020-02-19 14:31:53 +0900

    pc방 아이디 찾기화면 pc방 비밀번호 찾기화면 pc방 비밀번호 재설정 찾기화면

---
+++ app/src/main/java/com/ezen/MyPcApplication/After_Main/Find_Store_Tap/PC_Info/PC_Member/PcRoomIdFindActivity.java
@@ -0,0 +1,16 @@
+package com.ezen.MyPcApplication.After_Main.Find_Store_Tap.PC_Info.PC_Member;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ezen.MyPcApplication.R;
+
+public class PcRoomIdFindActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pc_room_id_find);
+    }
+}
---
+++ app/src/main/java/com/ezen/MyPcApplication/After_Main/Find_Store_Tap/PC_Info/PC_Member/PcRoomPwFindActivity.java
@@ -0,0 +1,16 @@
+package com.ezen.MyPcApplication.After_Main.Find_Store_Tap.PC_Info.PC_Member;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ezen.MyPcApplication.R;
+
+public class PcRoomPwFindActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pc_room_pw_find);
+    }
+}
